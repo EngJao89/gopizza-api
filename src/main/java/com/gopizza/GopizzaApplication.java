242a920c997b922f807ea7fd8131dd836076bfd1
EngJao89 <eng_joao@outlook.com>
2026-01-05 22:05:49 -0300

    create project for pizzaria  using spring boot.

---
+++ src/main/java/com/gopizza/GopizzaApplication.java
@@ -0,0 +1,13 @@
+package com.gopizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GopizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GopizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gopizza/GopizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gopizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GopizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
